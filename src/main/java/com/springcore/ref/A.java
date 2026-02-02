d537182e757f80a3547e7e9213b23158481085ce
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2026-02-02 16:29:17 +0530

    first commit

---
+++ src/main/java/com/springcore/ref/A.java
@@ -0,0 +1,28 @@
+package com.springcore.ref;
+
+public class A {
+	private int a;
+	private B obj;
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public B getObj() {
+		return obj;
+	}
+	public void setObj(B obj) {
+		this.obj = obj;
+	}
+	@Override
+	public String toString() {
+		return "A [a=" + a + ", obj=" + obj + "]";
+	}
+	public A() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
