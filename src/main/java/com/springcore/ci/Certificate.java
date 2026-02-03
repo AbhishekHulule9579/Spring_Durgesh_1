01617983156380cda639739f2135f0803151da1b
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2026-02-03 08:53:07 +0530

    constructor injection upgrade

---
+++ src/main/java/com/springcore/ci/Certificate.java
@@ -0,0 +1,16 @@
+package com.springcore.ci;
+
+public class Certificate {
+	String name;
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return this.name;
+	}
+
+	public Certificate(String name) {
+		super();
+		this.name = name;
+	}
+}
