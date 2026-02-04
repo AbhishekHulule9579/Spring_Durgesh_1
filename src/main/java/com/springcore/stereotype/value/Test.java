a4df1472ae5f9ea6857099a67846668466cf86ae
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2026-02-04 19:06:02 +0530

    @Value annotation for multiple value(list,vector)

---
+++ src/main/java/com/springcore/stereotype/value/Test.java
@@ -0,0 +1,17 @@
+package com.springcore.stereotype.value;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext context=new ClassPathXmlApplicationContext
+				("com/springcore/stereotype/value/stereoconfig.xml");
+		Student student=context.getBean("student",Student.class);
+		System.out.println(student);
+		System.out.println(student.getAddress());
+		System.out.println(student.getAddress().getClass().getName());
+	}
+
+}
